f69a7650fc2b9c056110b4a80f255ff027f4d31c
Ashish Raj <140612438+AshishRaj360@users.noreply.github.com>
2024-11-07 06:55:43 +0000

    container config done

---
+++ 1_javaScript/3_conversionOperation.js
@@ -0,0 +1,31 @@
+let score ="33abc"
+
+console.log (typeof score);
+console.log (typeof (score));
+
+let valueInNumber = Number (score)
+console.log (typeof valueInNumber);
+console.log (valueInNumber);
+
+
+// "33"- 33
+// "33abc"
+//null
+//undefined 
+//true
+//String
+
+
+let isloggedin =""
+let lin = Boolean(isloggedin)
+console.log(lin);
+
+// 1- true 
+// 0- false 
+//""- false
+//"hitesh"- true
+
+let sNumber = 33
+let stringNumber = String (sNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
---
+++ 1_javaScript/basics.js
@@ -0,0 +1,13 @@
+const accountId = 8392
+let accountEmail ="ar82750@gmail.com"
+accountCity  = "bhagalpur"
+var accountPassword = "7079660048"
+
+accountEmail= "bahvya420@gmail.com"
+accountPassword = "235538"
+accountCity = "bihar "
+
+console.log(accountEmail);
+consol.table([accountEmail,accountCity,accountId,accountPassword])
+
+// 
