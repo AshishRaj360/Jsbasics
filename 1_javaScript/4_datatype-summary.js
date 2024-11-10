c6e270536a63900b68758801cc4998b383faf25a
Ashish Raj <140612438+AshishRaj360@users.noreply.github.com>
2024-11-10 17:49:50 +0000

    data update

---
+++ 1_javaScript/4_datatype-summary.js
@@ -0,0 +1,22 @@
+// primitive datatype 
+// 7 types : String, Number, Boolean,Null,Undefined ,
+// symbool,Bigint (for large no of data)
+
+// NON  Primitive 
+
+// Arrays,object,function
+// array example 
+
+// const HERO = ["saktimaan ","choota Bheem "]
+
+// Object Example 
+
+// {
+// Name:"Ashish",
+// age : 28,
+// }
+
+// Function as variable 
+//  Const myfunction(){
+
+//  }
